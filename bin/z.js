fb1978f1e7154c2fbfb1efadd6cf9bd9fb0a516a
Guillermo de la Iglesia <zguillez@gmail.com>
2015-11-17 19:07:21 +0100

    shelljs global

--- bin/z.js
+++ bin/z.js
@@ -1,17 +1,17 @@
 #! /usr/bin/env node
 
-var shell = require("shelljs");
+require('shelljs/global');
 var param = process.argv[2];
 if (param === 'version' || param === '-v') {
 	var fs = require("fs");
 	var file = fs.readFileSync("package.json");
 	console.log(JSON.parse(file).version);
 } else if (param === 'alias') {
-	console.log('alias ls="clear && pwd && ls -lashF"');
+	console.log('alias ls="clear && pwd"');
 } else if (param === 'export') {
 	console.log('export PS1="\\u:\\W:$ "');
 } else if (param === 'ls') {
-	shell.exec('clear && pwd && ls -lashF');
+	exec('clear && pwd && ls -lashF');
 } else {
 	var msg = "\nUsage: z <command>\n\nwhere <command> is one of:\n\tversion, alias, export, ls\n";
 	console.log(msg);
