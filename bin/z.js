9d0ab9e5ed5673002f54d58c9d4435dfd68c80b5
Guillermo de la Iglesia <zguillez@gmail.com>
2015-11-17 18:49:40 +0100

    simple commands

--- bin/z.js
+++ bin/z.js
@@ -1,15 +1,18 @@
 #! /usr/bin/env node
-var shell = require("shelljs");
 
-switch(process.argv[2])
-{
-	case 'alias':
-		var command = 'alias ls="clear && pwd && ls -lashF"';
-		shell.exec('echo ' + command);
-		break;
-	case 'ls':
-		shell.exec('clear && pwd && ls -lashF');
-		break;
-	default:
-		shell.exec('echo "ERR: No params!"');
+var shell = require("shelljs");
+var param = process.argv[2];
+if (param === 'version' || param === '-v') {
+	var fs = require("fs");
+	var file = fs.readFileSync("package.json");
+	console.log(JSON.parse(file).version);
+} else if (param === 'alias') {
+	console.log('alias ls="clear && pwd && ls -lashF"');
+} else if (param === 'export') {
+	console.log('export PS1="\\u:\\W:$ "');
+} else if (param === 'ls') {
+	shell.exec('clear && pwd && ls -lashF');
+} else {
+	var msg = "\nUsage: z <command>\n\nwhere <command> is one of:\n\tversion, alias, export, ls\n";
+	console.log(msg);
 }
