e0dd36528de0b9b193b776f0b5d1b55f5326ee80
Guillermo de la Iglesia <zguillez@gmail.com>
2015-11-20 10:19:54 +0100

    du command

--- bin/z.js
+++ bin/z.js
@@ -14,7 +14,12 @@ if (param === 'version' || param === '-v') {
 	console.log('export PS1="\\u:\\W:$ "');
 } else if (param === 'ls') {
 	shell.exec('clear && pwd && ls -lashF');
+} else if (param === 'du') {
+	shell.exec('clear && pwd && du -csh * | sort -n');
+} else if (param === 'help' || param === '-h') {
+	var msg = "\nUsage: z <command>ls: clear && pwd && ls -lashFdu: clear && pwd && du -csh * | sort -n\n";
+	console.log(msg);
 } else {
-	var msg = "\nUsage: z <command>\n\nwhere <command> is one of:\n\tversion, alias, export, ls\n";
+	var msg = "\nUsage: z <command>\n\nwhere <command> is one of:\n\tversion, alias, export, ls, du\n";
 	console.log(msg);
 }
