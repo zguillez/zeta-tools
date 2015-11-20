6b11cd5302772f452c812244cb9387e7b08871f0
Guillermo de la Iglesia <zguillez@gmail.com>
2015-11-20 21:49:23 +0100

    colors

--- bin/z.js
+++ bin/z.js
@@ -3,18 +3,31 @@
 var shell = require('shelljs');
 var fs = require('fs');
 var path = require('path');
+var colors = require('colors');
 var param1 = process.argv[2];
 var param2 = process.argv[3];
 var dir = path.resolve(__dirname, '../');
 var pwd = process.env.PWD;
+colors.setTheme({
+	silly: 'rainbow',
+	input: 'grey',
+	verbose: 'cyan',
+	prompt: 'grey',
+	info: 'green',
+	data: 'grey',
+	help: 'cyan',
+	warn: 'yellow',
+	debug: 'blue',
+	error: 'red'
+});
 //---------------------------------------------------------
 if (param1 === 'version' || param1 === '-v') {
 	var file = fs.readFileSync(dir + "/package.json");
 	console.log(JSON.parse(file).version);
 } else if (param1 === 'alias') {
-	console.log('alias ls="clear && pwd && ls -lashF"');
+	console.log('alias ls="clear && pwd && ls -lashF"'.info);
 } else if (param1 === 'export') {
-	console.log('export PS1="\\u:\\W:$ "');
+	console.log('export PS1="\\u:\\W:$ "'.info);
 } else if (param1 === 'ls') {
 	shell.exec('clear && pwd && ls -lashF');
 } else if (param1 === 'du') {
@@ -45,13 +58,13 @@ if (param1 === 'version' || param1 === '-v') {
 	shell.exec('sudo npm i -g zeta-tools');
 } else if (param1 === 'help' || param1 === '-h') {
 	shell.exec('clear');
-	console.log('*******************************************');
+	console.log('*******************************************'.prompt);
 	var msg =
 		"\nUsage: z <command>\n\nself-update: sudo npm i -g zeta-tools (use -f for unistall first)\nupdate: sudo ncu -u && sudo ncu -m bower -u\nls: clear && pwd && ls -lashF\ndu: clear && pwd && du -hsc * | sort -hr \n    clear && pwd && du -hsc * | gsort -hr (osx) \n\n>Update NodeJS\nsudo npm cache clean -f\nsudo npm install -g n\nsudo n stable\n\n>Requeriments for OSX:\nbrew install coreutils\n\n>Uninstall\nsudo npm un -g zeta-tools\n..or..\nsudo npm cache clean\nsudo rm -rf /usr/local/lib/node_modules/zeta-tools\n";
-	console.log(msg);
-	console.log('*******************************************');
+	console.log(msg.help);
+	console.log('\n*******************************************\n'.prompt);
 } else {
 	var msg =
 		"\nUsage: z <command>\n\nwhere <command> is one of:\n\tversion -v, help -h, self-update, update -u, alias, export, ls, du\n";
-	console.log(msg);
+	console.log(msg.warn);
 }
