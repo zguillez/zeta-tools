61ab8e932a7103f551a0de9d73509b19a911fa9e
Guillermo de la Iglesia <zguillez@gmail.com>
2016-09-09 13:41:53 +0200

    fix git tag

--- bin/git.js
+++ bin/git.js
@@ -4,15 +4,15 @@ var param3 = process.argv[4];
 var param4 = process.argv[5];
 var msg = '';
 if (param2) {
+	console.log('git add --all'.info);
+	shell.exec('git add --all && git commit -m "' + param2 + '"');
+	msg = 'git commit -m "' + param2 + '"';
+	console.log(msg.warn);
 	if (param3 && param3 != '') {
 		msg = 'git tag -a ' + param3 + ' -m "' + param3 + '"';
 		console.log(msg.info);
 		shell.exec('git tag -a ' + param3 + ' -m "' + param3 + '"');
 	}
-	console.log('git add --all'.info);
-	shell.exec('git add --all && git commit -m "' + param2 + '"');
-	msg = 'git commit -m "' + param2 + '"';
-	console.log(msg.warn);
 	if (param4) {
 		msg = 'git push -u origin ' + param4 + ' --tags';
 		console.log(msg.info);
